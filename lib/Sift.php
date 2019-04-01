ffc964516e66330bca416295a71800d3324a381f
David Ehrmann <dehrmann@siftscience.com>
2019-04-01 14:40:41 -0700

    Update VERSION constant

--- lib/Sift.php
+++ lib/Sift.php
@@ -2,25 +2,25 @@
 
 abstract class Sift
 {
-	/**
-	 *	@var string the API key to be used for requests if none is provided to the constructor.
-	 */
-	public static $api_key;
+    /**
+     *	@var string the API key to be used for requests if none is provided to the constructor.
+     */
+    public static $api_key;
 
         /**
-	 *	@var string the account ID to be used for requests if none is provided to the constructor.
-	 */
-	public static $account_id;
+     *	@var string the account ID to be used for requests if none is provided to the constructor.
+     */
+    public static $account_id;
 
-	const VERSION = '3.1.0';
+    const VERSION = '4.0.0';
 
-	public static function setApiKey($api_key)
-	{
-		self::$api_key = $api_key;
-	}
+    public static function setApiKey($api_key)
+    {
+        self::$api_key = $api_key;
+    }
 
-	public static function setAccountId($account_id)
-	{
-		self::$account_id = $account_id;
-	}
+    public static function setAccountId($account_id)
+    {
+        self::$account_id = $account_id;
+    }
 }
