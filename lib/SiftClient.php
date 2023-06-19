159a69198c7e8a4e772f2255e3e6cc77e6a0ad1b
haneeshv <135820493+haneeshv@users.noreply.github.com>
2023-06-19 18:28:07 +0530

    Support Score Percentiles in response (#99)
    Support score percentiles param

--- lib/SiftClient.php
+++ lib/SiftClient.php
@@ -116,8 +116,10 @@ class SiftClient {
             'abuse_types' => [],
             'path' => null,
             'timeout' => $this->timeout,
-            'version' => $this->version
+            'version' => $this->version,
+            'include_score_percentiles' => false
         ]);
+
         $this->validateArgument($event, 'event', 'string');
         $this->validateArgument($properties, 'properties', 'array');
 
@@ -142,7 +144,9 @@ class SiftClient {
             $params['force_workflow_run'] = 'true';
         if ($opts['abuse_types'])
             $params['abuse_types'] = implode(',', $opts['abuse_types']);
-
+        if($opts['include_score_percentiles'])
+            $params['fields'] = 'SCORE_PERCENTILES';
+            
         try {
             $request = new SiftRequest(
                  $path, SiftRequest::POST, $opts['timeout'], $opts['version'], [
